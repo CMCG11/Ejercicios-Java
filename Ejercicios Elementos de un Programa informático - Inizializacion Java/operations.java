b612a3527d9478657d3de9b26ea33f115f139257
Celya <celia.castilla@gmail.com>
2019-11-05 11:05:54 +0000

    Add files via upload

---
+++ Ejercicios Elementos de un Programa informático - Inizializacion Java/operations.java
@@ -0,0 +1,22 @@
+public class Operations{
+	public static void main(String[] args){
+	/*Definimos las variables*/
+	int a=12;
+	int b=15;
+	int suma=a+b;
+	int resta=a-b;
+	int multiplicacion=a*b;
+	double division=a/b;
+	int modulo=a%b;
+	boolean mayor=a>b;
+
+	/*Mostramos el resultado por pantalla*/
+	System.out.println("El resultado de la suma es "+(suma));
+	System.out.println("El resultado de la resta es "+(resta));
+	System.out.println("El resultado de la multiplicacion es "+(multiplicacion));
+	System.out.println("El resultado de la division es "+(division));
+	System.out.println("El resultado del modulo es "+(modulo));
+	System.out.println("Que a sea mayor que b "+(mayor));
+	}
+}
+
